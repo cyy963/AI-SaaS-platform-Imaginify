a05cefedc6aa31b4c32357f94f99f1ccd32cc10d
cyy963 <cyrusw89@gmail.com>
2024-11-27 15:47:27 +1100

    build fix: ESlint and type errors

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,8 @@
-import { UserButton } from "@clerk/nextjs"
-
 const Home = () => {
   return (
     <div>
       <p>Home</p>
-      
     </div>
-  )
-}
-export default Home
+  );
+};
+export default Home;
